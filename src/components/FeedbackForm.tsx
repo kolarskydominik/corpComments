93611b5511faa83e45fa17014fd212e28f2afc71
Dominik Kolarsky <kolarskydominik@gmail.com>
2024-03-28 13:55:34 +0800

    added: ui compoents

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,3 +1,22 @@
 export default function FeedbackForm() {
-  return <>FeedbackForm</>;
+  return (
+    <>
+      <form className="form ">
+        <textarea
+          id="feedback-textarea"
+          placeholder="blabla"
+          spellCheck={false}
+        ></textarea>
+        <label htmlFor="feedback-textarea">
+          Enter your feedback here, remember to #hashtag to company
+        </label>
+        <div>
+          <p className="u-italic">150</p>
+          <button>
+            <span>Submit</span>
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,3 +1,25 @@
+import { TriangleUpIcon } from '@radix-ui/react-icons';
+
 export default function FeedbackList() {
-  return <ol className="feedback-list">FeedbackList</ol>;
+  return (
+    <ol className="feedback-list">
+      <li className="feedback">
+        <button>
+          <TriangleUpIcon />
+          <span>543</span>
+        </button>
+        <div>
+          <p>B</p>
+        </div>
+        <div>
+          <p>ByteGrad</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            nihil totam necessitatibus vero quod explicabo.
+          </p>
+        </div>
+        <p>4d</p>
+      </li>
+    </ol>
+  );
 }
