7c723019488426402340436724930cb23eadf753
Dominik Kolarsky <kolarskydominik@gmail.com>
2024-03-28 20:29:19 +0800

    added: upvote btn logic

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,16 +1,29 @@
 import { TriangleUpIcon } from '@radix-ui/react-icons';
 import { FeedbackItemType } from '../../lib/types';
+import { useState } from 'react';
 
 type FeedbackItemProps = {
   feedbackItem: FeedbackItemType;
 };
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
+  const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setUpvoteCount((prev) => ++prev);
+    e.currentTarget.disabled = true;
+    e.stopPropagation();
+  };
+
   return (
-    <li className="feedback">
-      <button>
+    <li
+      onClick={() => setOpen((prev) => !prev)}
+      className={`feedback ${open ? 'feedback--expand' : ''}`}
+    >
+      <button onClick={handleClick}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
       <div>
         <p>{feedbackItem.badgeLetter}</p>
