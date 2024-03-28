cac9855a3b88c259b24f9a870aaa539bd55d9e40
Dominik Kolarsky <kolarskydominik@gmail.com>
2024-03-28 14:19:15 +0800

    added: input logic

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
+import { MAX_CHARS } from './lib/constans';
+
 export default function FeedbackForm() {
+  const [text, setText] = useState('');
+  const charCount = MAX_CHARS - text.length;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (newText.length > MAX_CHARS) return;
+    setText(newText);
+  };
+
   return (
     <>
-      <form className="form ">
+      <form className="form">
         <textarea
+          value={text}
           id="feedback-textarea"
           placeholder="blabla"
           spellCheck={false}
+          onChange={handleChange}
         ></textarea>
         <label htmlFor="feedback-textarea">
           Enter your feedback here, remember to #hashtag to company
         </label>
         <div>
-          <p className="u-italic">150</p>
+          <p className="u-italic">{charCount}</p>
           <button>
             <span>Submit</span>
           </button>
